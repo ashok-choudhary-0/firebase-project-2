31829859c44f6f1580b7645e95b3b44a006e730f
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-10-10 17:28:16 +0530

    update createpost logic

--- controller/postController.js
+++ controller/postController.js
@@ -4,12 +4,9 @@ const slugify = require('slugify')
 const createPost = async (req, res) => {
   const bodyData = matchedData(req);
   try {
-    const getPostsDocRef = firestore().collection("posts").doc(bodyData.uid)
-    const userPosts = (await getPostsDocRef.get()).get("yourPosts")
-    const currentPosts = userPosts ? userPosts : [];
-    const updatedPosts = currentPosts.concat({ title: bodyData.title, description: bodyData.description, slug: slugify(bodyData.slug), updatedAt: new Date(), createdAt: new Date(), updatedBy: bodyData.uid });
-    await getPostsDocRef.update({
-      yourPosts: updatedPosts
+    const postDoc= firestore().collection("posts").doc()
+    await postDoc.set({
+      title: bodyData.title, description: bodyData.description, slug: slugify(bodyData.slug), updatedAt: new Date(), createdAt: new Date(), updatedBy: bodyData.uid
     })
     res.status(200).send({ message: "Post created successfully" })
   } catch (err) {
